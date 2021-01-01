b379a0248901a97a73ea331914d728079d427a18
Abdelrahman Ali <abdelrahman.ali@rtl.nl>
2021-01-01 22:20:54 +0100

    Initial Commit

---
+++ odiggo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odiggo
+//
+//  Created by Abdelrahman Ali on 01/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
